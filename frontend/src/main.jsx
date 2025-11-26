727e7ad7a47596279a34960881ba893e9b678af3
Skand Gaur <skand.gaur@healthsetgo.com>
2025-11-26 20:02:24 +0530

    hashrouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import ThemeProvider from '../theme/ThemeProvider';
 import './styles.css';
@@ -8,9 +8,9 @@ import './styles.css';
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
